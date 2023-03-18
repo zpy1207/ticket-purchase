7c1b0a46a0357625385d0fbf7a8487e3f369bc7e
Perry Zeng <961992589@qq.com>
2023-03-18 21:00:31 +0800

    add choosen-card

---
+++ components/ChoosenTicketCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="choosen-ticket-card ticketCardLayout">
+    <button class="button-close" @click="deleteOption">
+      <svg t="1678710572127" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2798" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32"><path d="M570 510.8l254.9-255.1c16.4-16.5 16.4-43 0-59.5s-43-16.5-59.5 0L510.5 451.3 255.8 196.2c-16.4-16.5-43-16.5-59.5 0-16.4 16.5-16.4 43 0 59.5l254.9 255.1-254.9 255c-16.4 16.5-16.4 43 0 59.5 8.1 8.2 18.9 12.4 29.7 12.4s21.5-4.1 29.7-12.4l254.7-255 254.8 255c8.1 8.2 19 12.4 29.7 12.4 10.8 0 21.5-4.1 29.6-12.4 16.4-16.5 16.4-43 0-59.5L570 510.8z m0 0" p-id="2799"></path></svg>
+    </button>
+    <div class="content">
+      <div class="company itrTxtPair">
+        <span class="bt_focusText">
+          公司名-飞机编号
+        </span>
+        <span class="bt_focusText"> 航班编号 </span>
+      </div>
+      <div class="time itrTxtPair">
+        <div class="bt_focusText">startTime - endTime</div>
+        <div class="duration">duration</div>
+      </div>
+      <div class="ticket-detail">
+        <span class="bt_focusText">first-class</span>
+        <span> × </span>
+        <span class=""> 1 </span>
+      </div>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChoosenTicketCard',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    deleteOption() {
+      console.log('delete')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/components/choosenTicketCard.scss"
+</style>
--- pages/searchDetail.vue
+++ pages/searchDetail.vue
@@ -2,6 +2,7 @@
   <div class="search-bar">
     <header-tab></header-tab>
 
+    <!-- search-bar -->
     <div class="relative sticky-search-position">
       <div class="a-container">
         <div
@@ -14,9 +15,20 @@
 
     <div class="mainLayout flex">
       <div class="choosePane" style="">
-        <div class="choose-pane-container" style=""></div>
-        <!-- choose-pane -->
+        <div class="choose-pane-container" style="">
+          <div class="choose-pane-title">
+            <span class="icon-plane">
+              <i class="el-icon-s-promotion"></i>
+            </span>
+            <span>已选择航班：</span>
+          </div>
+
+
+          <choosen-ticket-card></choosen-ticket-card>
+        </div>
       </div>
+
+      <!-- ticket-list -->
       <div class="flex flex-column" style="flex-direction: column; flex: 1;">
         <div
           v-for="(item, index) in detailList"
@@ -38,10 +50,12 @@
 import SearchTab from '../components/SearchTab.vue'
 import HeaderTab from '~/components/HeaderTab.vue'
 import TicketCard from '~/components/TicketCard.vue'
+import ChoosenTicketCard from '~/components/ChoosenTicketCard.vue'
 
 export default {
   name: 'SearchDetailPage',
   comments: {
+    ChoosenTicketCard,
     HeaderTab,
     SearchTab,
     TicketCard
