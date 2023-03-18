ad72b43fb7ba085656be2a847c1cd860d567877b
Perry Zeng <961992589@qq.com>
2023-03-18 17:25:15 +0800

    add choose modal

--- components/TicketCard.vue
+++ components/TicketCard.vue
@@ -94,6 +94,74 @@
       </div>
     </div>
 
+    <div
+      v-if="showModal"
+      class="choose-modal a-modal-container"
+    >
+      <span class="a-modal-backdrop"></span>
+      <div class="a-card a-modal is-full auth-modal" style="width: 600px; z-index: 800;">
+        <div class="a-card__body">
+          <button class="btn is-md is-raw auth-modal-close p-3 m-3" @click="closeModal">
+            <svg t="1678710572127" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2798" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32"><path d="M570 510.8l254.9-255.1c16.4-16.5 16.4-43 0-59.5s-43-16.5-59.5 0L510.5 451.3 255.8 196.2c-16.4-16.5-43-16.5-59.5 0-16.4 16.5-16.4 43 0 59.5l254.9 255.1-254.9 255c-16.4 16.5-16.4 43 0 59.5 8.1 8.2 18.9 12.4 29.7 12.4s21.5-4.1 29.7-12.4l254.7-255 254.8 255c8.1 8.2 19 12.4 29.7 12.4 10.8 0 21.5-4.1 29.6-12.4 16.4-16.5 16.4-43 0-59.5L570 510.8z m0 0" p-id="2799"></path></svg>
+          </button>
+            <h3 class="h3">
+              <font style="vertical-align: inherit;">请选择舱位和票数</font>
+            </h3>
+            <div class="flex">
+              <span class="modal-label">舱位：</span>
+              <div class="a-input mb-5 text-right has-append is-lg" style="flex: 1">
+                <span class="a-input__input">
+                  <select
+                    v-model="selectedClass"
+                  >
+                    <option value=''>请选择舱位</option>
+                    <option value ="firstClass">头等舱</option>
+                    <option value ="secondClass">商务舱</option>
+                    <option value="economyClass">经济舱</option>
+                  </select>
+                </span>
+                <!-- <fieldset>
+                  <legend style="width: 68px;"></legend>
+                </fieldset> -->
+                <label class="a-input__label">
+                  <font style="vertical-align: inherit;"></font>
+                </label>
+                <span class="help-text is-invalid">
+                  <font style="vertical-align: inherit;"></font>
+                </span>
+              </div>
+            </div>
+
+            <div class="flex">
+              <span class="modal-label">票数：</span>
+              <div class="a-input mb-5 text-right has-append is-lg" style="flex: 1">
+                <span class="a-input__input">
+                  <input
+                    v-model="numPeople"
+                    type="number"
+                  >
+                </span>
+                <!-- <fieldset>
+                  <legend style="width: 68px;"></legend>
+                </fieldset> -->
+                <label class="a-input__label">
+                  <font style="vertical-align: inherit;"></font>
+                </label>
+                <span class="help-text is-invalid">
+                  <font style="vertical-align: inherit;"></font>
+                </span>
+              </div>
+              <span class="modal-label" style="padding-left: 10px;">张</span>
+            </div>
+
+            <div class="text-center auth-actions">
+              <button class="btn is-lg is-solid-secondary is-block is-disabled py-3 mb-4" @click="onSubmit">
+                <font style="vertical-align: inherit;">确认选择</font>
+              </button>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -102,7 +170,10 @@ export default {
   name: 'TicketCard',
   data() {
     return {
-      showDetail: false
+      numPeople: 1,
+      selectedClass: '',
+      showDetail: false,
+      showModal : false,
     }
   },
   computed: {
@@ -111,12 +182,20 @@ export default {
     }
   },
   methods: {
+    closeModal() {
+      console.log('close')
+      this.showModal = false
+    },
     onChoose() {
+      this.showModal = true
       console.log('choose')
     },
     onCollapse() {
       console.log(this.showDetail)
       this.showDetail = !this.showDetail
+    },
+    onSubmit() {
+      console.log(this.selectedClass)
     }
   }
 }
